105d6e0bd696861cd0ef124bc7b606197843605c
Pelckie1978 <c_pelckmans@telenet.be>
2026-01-13 22:10:58 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('sinksen-v1').then(cache => cache.addAll([
+    caches.open('sinksen-v2').then(cache => cache.addAll([
       './','./index.html','./manifest.json','./service-worker.js','./icon512.png'
     ]))
   );
 });
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
